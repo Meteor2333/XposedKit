1daa56424e049d1ccd0d12a410f396688eb03918
Meteor2333 <Meteor23333@outlook.com>
2026-06-23 19:32:08 +0800

    Initial commit

---
+++ xposed-kit/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "cc.meteormc.xposedkit"
+
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
