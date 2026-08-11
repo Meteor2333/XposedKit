05f62b2f03b69836154ce9c034e38b6b75c582ae
Meteor2333 <Meteor23333@outlook.com>
2026-08-11 12:08:55 +0800

    refactor: 修改hook方法返回类型为HookHandle而不是链式返回值

--- xposedkit/src/main/java/cc/meteormc/xposedkit/hook/HookerContext.kt
+++ xposedkit/src/main/java/cc/meteormc/xposedkit/hook/HookerContext.kt
@@ -23,30 +23,29 @@ open class HookerContext(
         return classLoader.typedReflect(this, block)
     }
 
-    fun Member.hook(type: HookType, priority: Int = InvokeCallback.PRIORITY_NORMAL, callback: InvokeCallback): Member {
+    fun Member.hook(type: HookType, priority: Int = InvokeCallback.PRIORITY_NORMAL, callback: InvokeCallback): HookHandle {
         val handle = XposedKit.impl.hook(this, type, priority, callback)
         handles.getOrPut(this) { mutableListOf() } += handle
-        return this
+        return handle
     }
 
-    fun Member.hookBefore(priority: Int = InvokeCallback.PRIORITY_NORMAL, callback: InvokeCallback): Member {
+    fun Member.hookBefore(priority: Int = InvokeCallback.PRIORITY_NORMAL, callback: InvokeCallback): HookHandle {
         return hook(HookType.BEFORE, priority, callback)
     }
 
-    fun Member.hookAfter(priority: Int = InvokeCallback.PRIORITY_NORMAL, callback: InvokeCallback): Member {
+    fun Member.hookAfter(priority: Int = InvokeCallback.PRIORITY_NORMAL, callback: InvokeCallback): HookHandle {
         return hook(HookType.AFTER, priority, callback)
     }
 
-    fun <T : Iterable<Member>> T.hook(type: HookType, callback: InvokeCallback): T {
-        forEach { it.hook(type, InvokeCallback.PRIORITY_NORMAL, callback) }
-        return this
+    fun <T : Iterable<Member>> T.hook(type: HookType, callback: InvokeCallback): List<HookHandle> {
+        return map { it.hook(type, InvokeCallback.PRIORITY_NORMAL, callback) }
     }
 
-    fun <T : Iterable<Member>> T.hookBefore(callback: InvokeCallback): T {
+    fun <T : Iterable<Member>> T.hookBefore(callback: InvokeCallback): List<HookHandle> {
         return hook(HookType.BEFORE, callback)
     }
 
-    fun <T : Iterable<Member>> T.hookAfter(callback: InvokeCallback): T {
+    fun <T : Iterable<Member>> T.hookAfter(callback: InvokeCallback): List<HookHandle> {
         return hook(HookType.AFTER, callback)
     }
 }
