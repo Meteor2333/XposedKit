df026fccc23e60e800ed0b2bed64a9595dfe9a67
Meteor2333 <Meteor23333@outlook.com>
2026-08-19 16:07:01 +0800

    refactor: 优化InvokeInfo参数查找和泛型

--- xposedkit/src/main/java/cc/meteormc/xposedkit/hook/InvokeInfo.kt
+++ xposedkit/src/main/java/cc/meteormc/xposedkit/hook/InvokeInfo.kt
@@ -25,59 +25,51 @@ class InvokeInfo(
 
     fun <T> instance() = this.instance as T
 
-    fun booleanArg(index: Int = 0) = argByGenerics<Boolean>(index)
-    fun byteArg(index: Int = 0) = argByGenerics<Byte>(index)
-    fun charArg(index: Int = 0) = argByGenerics<Char>(index)
-    fun doubleArg(index: Int = 0) = argByGenerics<Double>(index)
-    fun floatArg(index: Int = 0) = argByGenerics<Float>(index)
-    fun intArg(index: Int = 0) = argByGenerics<Int>(index)
-    fun longArg(index: Int = 0) = argByGenerics<Long>(index)
-    fun shortArg(index: Int = 0) = argByGenerics<Short>(index)
-    fun stringArg(index: Int = 0) = argByGenerics<String>(index)
-    fun <T> argByClass(type: Class<T>, index: Int = 0) = this.args.filterIsInstance(type)[index]
-    inline fun <reified T> argByGenerics(index: Int = 0) = this.args.filterIsInstance<T>()[index]
+    fun <T> argumentAt(index: Int) = this.args[index] as T
 
-    fun booleanArg(value: Boolean, index: Int = 0) = argByGenerics<Boolean>(value, index)
-    fun byteArg(value: Byte, index: Int = 0) = argByGenerics<Byte>(value, index)
-    fun charArg(value: Char, index: Int = 0) = argByGenerics<Char>(value, index)
-    fun doubleArg(value: Double, index: Int = 0) = argByGenerics<Double>(value, index)
-    fun floatArg(value: Float, index: Int = 0) = argByGenerics<Float>(value, index)
-    fun intArg(value: Int, index: Int = 0) = argByGenerics<Int>(value, index)
-    fun longArg(value: Long, index: Int = 0) = argByGenerics<Long>(value, index)
-    fun shortArg(value: Short, index: Int = 0) = argByGenerics<Short>(value, index)
-    fun stringArg(value: String, index: Int = 0) = argByGenerics<String>(value, index)
-    fun <T> argByClass(value: Any, type: Class<T>, index: Int = 0) = this.args.withIndex()
-        .filter { type.isInstance(it.value) }[index]
-        .index
-        .let { args[it] = value }
-    inline fun <reified T> argByGenerics(value: Any, index: Int = 0) = this.args.withIndex()
-        .filter { it.value is T }[index]
-        .index
-        .let { args[it] = value }
+    fun <T> findArg(type: Class<T>, occurrence: Int = 0): T? {
+        return findArgIndexed(type, occurrence).value
+    }
 
-    val booleanResult
-        get() = this.result as Boolean
-    val byteResult
-        get() = this.result as Byte
-    val charResult
-        get() = this.result as Char
-    val doubleResult
-        get() = this.result as Double
-    val floatResult
-        get() = this.result as Float
-    val intResult
-        get() = this.result as Int
-    val longResult
-        get() = this.result as Long
-    val shortResult
-        get() = this.result as Short
-    val stringResult
-        get() = this.result as String
+    fun <T> findArgIndexed(type: Class<T>, occurrence: Int = 0): IndexedValue<T?> {
+        var count = 0
+        val paramTypes = when (member) {
+            is Constructor<*> -> member.parameterTypes
+            is Method -> member.parameterTypes
+            else -> throw IllegalStateException(/* This should not happen */)
+        }
+        for ((i, param) in paramTypes.withIndex()) {
+            val argument = args[i]
+            if (!type.isInstance(argument) && !type.isAssignableFrom(param)) continue
+            if (count++ == occurrence) {
+                return IndexedValue(i, args[i] as T?)
+            }
+        }
+
+        throw NoSuchElementException()
+    }
+
+    inline fun <reified T : Any> findArg(occurrence: Int = 0): T {
+        return findArgIndexed<T>(occurrence).value
+    }
+
+    inline fun <reified T : Any> findArgIndexed(occurrence: Int = 0): IndexedValue<T> {
+        var count = 0
+        for (argument in args.withIndex()) {
+            if (argument.value !is T) continue
+            if (count++ == occurrence) {
+                return argument as IndexedValue<T>
+            }
+        }
+
+        throw NoSuchElementException()
+    }
+
     fun <T> result() = this.result as T
 
-    fun <T : Throwable> exception() = this.exception as T
+    fun <T : Throwable?> exception() = this.exception as T
 
-    fun doNothing() {
+    fun cancel() {
         result = null
     }
 
